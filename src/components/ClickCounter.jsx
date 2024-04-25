deea3e930ecdb36ab6c5dff48e15ceb943e9857d
Arman-Kabir <armankabir8@gmail.com>
2024-04-25 12:35:41 +0600

    render props few confusions

--- src/components/ClickCounter.jsx
+++ src/components/ClickCounter.jsx
@@ -1,20 +1,12 @@
 import React from "react";
 
 export default class ClickCounter extends React.Component {
-  state = {
-    count: 0,
-  };
-
-  incrementCount = () => {
-    this.setState((prevState) => ({
-      count: prevState.count + 1,
-    }));
-  };
+ 
   render() {
-    const { count } = this.state;
+    const { count,incrementCount } = this.props;
     return (
       <div>
-        <button type="button" onClick={this.incrementCount}>
+        <button type="button" onClick={incrementCount}>
           clicked {count} times
         </button>
       </div>
